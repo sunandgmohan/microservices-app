e38eb67caf933cdc7ac761ccd982193619132717
sunandgmohan <sunandgmohan@gmail.com>
2026-04-27 09:38:19 +0530

    Update app.js

--- user-service/app.js
+++ user-service/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("USER SERVICE RUNNING");
+app.get('/users', (req, res) => {
+    res.json([{ id: 1, name: "Sunand" }]);
 });
 
 app.listen(3000, () => console.log("User Service running"));
